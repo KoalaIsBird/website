90c083d4f108f93919e387b4b83f743786b0e532
André <koalaisbird@gmail.com>
2025-06-05 19:52:36 +0200

    better website (not really)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <p>
+      <div className="flex justify-center items-center h-screen">
+        <p className="bg-red-600 w-64 text-amber-300 font-bold justify-center flex h-15 items-center rounded-4xl animate-bounce">
           Andrew's super cool website
         </p>
+        
       </div>
     </>
   )
